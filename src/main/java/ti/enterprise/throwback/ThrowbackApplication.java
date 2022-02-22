7b8b0048202368d9c3e388c500ffe8f084d23096
Dhimas <dhimas.ra48@gmail.com>
2022-02-22 14:18:17 +0700

    first

---
+++ src/main/java/ti/enterprise/throwback/ThrowbackApplication.java
@@ -0,0 +1,13 @@
+package ti.enterprise.throwback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrowbackApplication.class, args);
+	}
+
+}
